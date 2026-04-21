de9da53c9bebdbf1b12c3ccb24d8e2cbe8c6cdc2
Ducksteron <ducksteron@gmail.com>
2026-04-21 14:49:56 +0200

    fix: made getting data from db into one big query

--- managers/sql_wrapper.py
+++ managers/sql_wrapper.py
@@ -113,9 +113,40 @@ def get_stats_from_db(player_id: int, game_id:int) -> dict:
         cursor = connection.cursor()
         return_dict: dict = {}
         game_get_query = f"""
-        SELECT collected_sun, survived_time, plants_eaten, zombies_killed
-        FROM game
-        WHERE id = {game_id};
+        SELECT 
+            CAST(game.collected_sun AS BIGINT) AS "collected sun", 
+            CAST(game.survived_time AS BIGINT) AS "survived time", 
+            game.plants_eaten, game.zombies_killed,
+            (SELECT 
+                zombie_type.name AS "most killed zombie"
+            FROM 
+                zombie
+                INNER JOIN zombie_type ON zombie.type = zombie_type.id
+            WHERE
+                zombie.game = game.id
+            GROUP BY
+                zombie_type.name
+            ORDER BY
+                COUNT(zombie_type.name) DESC
+            LIMIT 1),
+
+            (SELECT 
+                plant_type.name AS "favorite plant"
+            FROM 
+                plant
+                INNER JOIN plant_type ON plant.type = plant_type.id
+            WHERE
+                plant.game = game.id
+            GROUP BY
+                plant_type.name
+            ORDER BY
+                COUNT(plant_type.name) DESC
+            LIMIT 1)
+            
+        FROM 
+            game
+        WHERE
+            game.id = {game_id};
         """
 
         cursor.execute(game_get_query)
@@ -125,52 +156,11 @@ def get_stats_from_db(player_id: int, game_id:int) -> dict:
         return_dict["survived time"] = cursor_results[0][1]
         return_dict["plants eaten"] = cursor_results[0][2]
         return_dict["zombies_killed"] = cursor_results[0][3]
-
-
-
-        zombie_query: str = f"""
-        SELECT zombie_type.name FROM zombie INNER JOIN
-        zombie_type ON zombie.type = zombie_type.id
-        WHERE zombie.game = {game_id};"""
-        zombie_types_query: str = f"""
-        SELECT name FROM zombie_type;"""
-
-        return_dict["favorite zombie"] = get_favorite_type(cursor, zombie_types_query, zombie_query, "no zombies killed!")
-
-
-
-        plant_query: str = f"""
-        SELECT plant_type.name FROM plant INNER JOIN
-        plant_type ON plant.type = plant_type.id
-        WHERE plant.game = {game_id};"""
-        plant_types_query: str = f"""
-        SELECT name FROM plant_type;"""
-
-        return_dict["favorite plant"] = get_favorite_type(cursor, plant_types_query, plant_query, "no plants planted!")
-
+        return_dict["favorite zombie"] = cursor_results[0][4]
+        return_dict["favorite plant"] = cursor_results[0][5]
 
         return return_dict
 
-def get_favorite_type(cursor, type_query: str, member_query: str, error_message: str) -> str:
-    favorite_type: dict = {"type": "NO FAVORITE FOUND", "amount": -1}
-    
-    member_list: list = []
-    cursor.execute(member_query)
-    member_list = tuple_list_to_list(cursor.fetchall()) 
-
-    type_list: list = []
-    cursor.execute(type_query)
-    type_list = tuple_list_to_list(cursor.fetchall()) 
-    
-    for type in type_list:
-        count_of_this_type = member_list.count(type)
-        if favorite_type["amount"] < count_of_this_type:
-            favorite_type["type"] = type
-            favorite_type["amount"] = count_of_this_type
-    
-    if favorite_type["type"] == "NO FAVORITE FOUND":
-            favorite_type["type"] = "no plants planted!"
-    return favorite_type["type"]
 
 def tuple_list_to_list(tuple_list) -> list:
     return_list: list = []
@@ -204,3 +194,7 @@ def gen_test_game_stats() -> GameStats:
 
 
     return new_game_stats
+
+def insert_buncha_stuff() -> None:
+     for i in range(20):
+          insert_stats(gen_test_game_stats())
